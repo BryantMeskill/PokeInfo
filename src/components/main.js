dfaa049928a3ce1f47dd9a3faf81563044d7015b
Bryant Meskill <bryant.meskill@gmail.com>
2022-12-09 10:49:28 -0600

    Automatically set class per Pokemon type.

--- src/components/main.js
+++ src/components/main.js
@@ -1,24 +1,47 @@
 import React from "react";
+import logo from "../img/logo.png";
 
+//helper function, go through the array of objects returned by API
+//and add each type name to output variable
 const formatType = (types) => {
   let output = "";
 
   types.forEach((obj) => {
-    output += `${obj.type.name} `;
+    output += `${obj.type.name.toUpperCase()} `;
   });
 
   return output;
 };
 
-function Main({ isLoading, name, dexNo, types, height, weight, sprite }) {
+function Main({
+  isLoading,
+  displayData,
+  name,
+  dexNo,
+  types,
+  height,
+  weight,
+  sprite,
+}) {
+  //typeList is for setting className of the 'type' span
+  //allows us to automatically generate color for pokemon type
+  let typeList = formatType(types);
   return (
-    <div className={`container ${isLoading ? "hidden" : ""}`}>
-      <img src={sprite} alt="Pic of searched Pokemon" className="sprite" />
-      <span className="name info">{name}</span>
-      <span className="dex info">PokeDex Number: {dexNo}</span>
-      <span className="type info">{formatType(types)}</span>
-      <span className="height info">Height: {height}m</span>
-      <span className="weight info">Weight {weight}kg</span>
+    <div className="Main">
+      <div className={`load ${isLoading ? "" : "hidden"}`}>
+        <img src={logo} alt="" />
+      </div>
+      <div className={`dataContainer ${displayData ? "hidden" : ""}`}>
+        <img src={sprite} alt="Searched Pokemon" className="sprite" />
+        <span className="name info">
+          {name} #{dexNo}
+        </span>
+        <span className={`type info ${typeList.toLowerCase()}`}>
+          {formatType(types)}
+        </span>
+        <span className="height info">Height: {height}m</span>
+        <span className="weight info">Weight {weight}kg</span>
+      </div>
     </div>
   );
 }
